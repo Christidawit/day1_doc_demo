84c56dc88789e312171319726648731bb7fdc586
Christian Woldemdhin <cwoldemdhin@middlebury.edu>
2019-07-01 11:58:47 -0700

    Remove and print the second element in the array

--- day1_doc/MyPlayground.playground/Contents.swift
+++ day1_doc/MyPlayground.playground/Contents.swift
@@ -1,5 +1,9 @@
 import UIKit
 
-var str = "Hello, playground"
-var arr = [1, 3, 5, 7]
+var str = "Hello, playground";
+var arr = [1, 3, 5, 7];
 // remove and print the 2nd element
+let second = arr[1];
+arr.remove(at: 1)
+print(second)
+
